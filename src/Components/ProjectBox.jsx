c3a6ba7e5583c2f392d71d7203e32122287c9c1a
Nacho <nachingast@gmail.com>
2024-05-10 18:08:11 -0300

    Agregado de sitios web, de cada respectivo proyecto

--- src/Components/ProjectBox.jsx
+++ src/Components/ProjectBox.jsx
@@ -6,15 +6,18 @@ const ProjectBox = ({ projectPhoto, projectName }) => {
   const desc = {
     FunkoPopDesc:
       "It's an ecommerce, the first one where I applied React. First practice with an API. Made for a Coderhouse course",
-    FunkoPopGithub: "https://github.com/Sehranagast/funko",
+    FunkoPopGithub: "https://github.com/Sehranagast/funko-store",
+    FunkoPopWebsite: "https://funkopop-store.netlify.app/",
 
     MoviesDesc:
       "This web app  is for helping you find your desired movie. I also learned new CSS properties and tricks.",
     MoviesGithub: "https://github.com/Sehranagast/Movie-app",
+    MoviesWebsite: "https://movies-century.netlify.app/",
 
     ColourPaletteDesc:
       "This website helps you generate random colours that you can use as a palette",
     ColourPaletteGithub: "https://github.com/Sehranagast/Colour-Palette",
+    ColourPaletteWebsite: "https://palettecolour.netlify.app/",
   };
 
   return (
@@ -30,6 +33,12 @@ const ProjectBox = ({ projectPhoto, projectName }) => {
         <a href={desc[projectName + "Github"]} target="_blank">
           <button className="projectbtn">
             <FaGithub /> Github
+          </button>
+        </a>
+
+        <a href={desc[projectName + "Website"]} target="_blank">
+          <button className="projectbtn">
+            <CgFileDocument /> Site
           </button>
         </a>
       </div>
